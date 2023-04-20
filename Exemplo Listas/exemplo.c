e272dab5669a25c49f8fa07ad40b5bd0754e51e9
João Pedro Scheffler <98850481+JP-Out@users.noreply.github.com>
2023-04-19 21:24:42 -0300

    Update and rename cxemplo.c to exemplo.c

--- Exemplo Listas/exemplo.c
+++ Exemplo Listas/exemplo.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct{
+typedef struct Node {
     int value;
     struct Node* next;
-
 } Node;
 
-void insertNode(int value, Node** head){
+void insertNode(int value, Node** head) {
     Node* newNode = (Node*) malloc(sizeof(Node));
 
     newNode->value = value;
@@ -16,18 +15,17 @@ void insertNode(int value, Node** head){
     *head = newNode; //deixando como primeiro elemento da lista
 }
 
-void printList(Node* head){ //funÁ„o para exibir os itens da lista
+void printList(Node* head) { //fun√ß√£o para exibir os itens da lista
     Node* current = head;
 
-    while(current != NULL){
-        printf("%d", current->value);//exibindo o item da lista
+    while (current != NULL) {
+        printf("%d ", current->value); //exibindo o item da lista
 
-        current = current->next;//indo para o proximo item da lista
+        current = current->next; //indo para o pr√≥ximo item da lista
     }
 }
 
-int main(){
-
+int main() {
     Node* head = NULL;
 
     insertNode(30, &head);
@@ -36,10 +34,14 @@ int main(){
     printf("\n");
 
     insertNode(60, &head);
+    printf("Lista: ");
     printList(head);
     printf("\n");
 
     insertNode(90, &head);
+    printf("Lista: ");
     printList(head);
     printf("\n");
+
+    return 0;
 }
